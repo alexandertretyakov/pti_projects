c4bcd7fa9ddc762b4bf4073a017f4bab5ade1340
igorbr <igor.bronovytskyi@playtech.com>
2021-05-18 22:59:42 +0300

    add to project js few tasks

---
+++ js/task_functions_part_4.js
@@ -0,0 +1,29 @@
+// Реализовать нижеуказанные функции.
+// ВНИМАНИЕ!
+// Соблюдайте форматирование кода (отступы, переносы)
+// Не использовать встроенные функции/методы
+
+
+
+
+// Создать функции size, last, getPositiveNumbers, without, min, sum, как методы массивов
+
+
+
+
+// Создать функции keys, values, pairs, isEmpty, extend, как методы объектов
+
+
+
+
+// Познакомиться с работой следующих втроенных свойств, функций и методов
+// Number
+//     Number.prototype: toFixed
+// String
+//     String.prototype: charAt, concat, includes, indexOf, lastIndexOf, repeat, replace, slice, split,
+//         substr, substring, toLowerCase, toUpperCase, trim
+// Array
+//     Array.prototype: concat, forEach, includes, indexOf, join, lastIndexOf, pop, push, reverse,
+//         shift, slice, splice, unshift, sort, map, filter, every, some, reduce, reduceRight
+// Object: keys, values, create, assign
+//     Object.prototype: hasOwnProperty
