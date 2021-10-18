9ad28452e47c854b311adcdf8651d13bb3bb4bb8
igorbr <igor.bronovytskyi@playtech.com>
2021-10-18 12:52:30 +0300

    add project: es6plus

---
+++ js/es6plus.js
@@ -0,0 +1,91 @@
+// Переписать определение функции используя значения параметров функции по умолчанию
+// var f = function(a, b) {
+//     a = a !== undefined ? a : 7;
+//     b = b !== undefined ? b : 4;
+//     console.log(a, b);
+// };
+
+
+
+
+// Переписать пример используя шаблоннные строки
+// var whore =
+//     '<div class="whore">'+
+//         '<div class="whore-name">' + Jasmine.name + '</div>'+
+//         '<img src="' + jasmine.photo + '" width="200">'+
+//         '<div class="whore-age">Возраст:' + jasmine.age + '</div>'+
+//         '<div class="whore-boobs">Размер груди: ' + jasmine.boobs + '</div>'+
+//         '<div class="whore-height">Рост: ' + jasmine.height + '</div>'+
+//         '<div class="whore-weight">Вес: ' + jasmine.weight + '</div>'+
+//         '<div class="whore-phone">' + jasmine.phone + '</div>'+
+//         '<div class="whore-can-come">Выезд: ' + (jasmine.can_come ? '+' : '-') + '</div>'+
+//         '<div class="whore-teaser">' + jasmine.teaser + '</div>'+
+//     '</div>';
+
+
+
+
+// Переписать определение объекта используя расширение объектных литералов
+var name = 'Вася';
+
+// var o = {
+//     name: name,
+//     getName: function() { return this.name }
+// };
+
+
+
+
+// Перепиши выражения используя оператор ...
+var whores = [
+    { name: 'Алиса', age: 20 },
+    { name: 'Богдана', age: 23 },
+    { name: 'Альбина', age: 21 }
+];
+
+var vasya = {
+    name: 'Вася',
+    age: 30
+};
+
+//whores = whores.concat({ name: 'Ванесса', age: 22 });
+
+// var fruits = ['orange', 'kiwi'].concat('cherry', 'banana');
+
+// var person = Object.assign({}, vasya, {
+//     age: 31
+// });
+
+
+
+
+// Перепиши определения функций используя стрелочные функции
+// var f1 = function() {};
+// var f2 = function(x) { return x*x; };
+// var f3 = function(a, b) {
+//     var c = a + b;
+//     return c*c;
+// };
+// var f4 = function() { return 'Хуй мусорам!'; };
+
+
+
+
+// Перепиши класс Circle из файла js/functions_part_5.js используя синтаксис классов
+
+
+
+
+// Перепиши инструкции определения переменных используя деструктурирующее присваивание
+var config = {
+    host: 'localhost',
+    port: 5984,
+    dbName: 'customers'
+};
+
+// var host = config.host;
+// var port2 = config.port;
+// var dbName = config.dbName !== undefined ? config.dbName : 'default' ;
+
+
+
