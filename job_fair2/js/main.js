26ffafe7a541005bf06534680b4397eff233ba28
igorbr <igor.bronovytskyi@playtech.com>
2021-11-05 18:25:15 +0200

    check project: job_fair2

--- job_fair2/js/main.js
+++ job_fair2/js/main.js
@@ -1,101 +1,141 @@
 var banner = {
-    slides: document.querySelectorAll('.banner-text'),
-    slidesLength: document.querySelectorAll('.banner-text').length,
+    el: document.querySelector('.module-banner'),
+    slides: document.querySelectorAll('.slide'),
     activeSlideIndex: 0,
-    next: document.querySelector('.next-banner'),
-    previous: document.querySelector('.previous-banner'),
+    nextBtn: document.querySelector('.next'),
+    previousBtn: document.querySelector('.previous'),
+    toggler: document.querySelector('.toggler'),
+    paginationButtons: document.querySelectorAll('.pagination button'),
 
-    init: function() {
-        this.next.addEventListener('click', function() {
-            console.log(banner.activeSlideIndex)
-            this.activeSlideIndex = this.activeSlideIndex === this.slidesLength - 1 ? 0 : this.activeSlideIndex + 1;
-            this.showSlide();
+    subscribe: function() {
+        this.nextBtn.addEventListener('click', this.handleClickOnNext.bind(this));
+        this.previousBtn.addEventListener('click', this.handleClickOnPrevious.bind(this));
+        this.paginationButtons.forEach(function(button) {
+            button.addEventListener('click', this.handleClickOnPaginationButton.bind(this))
         }.bind(this));
-
-        this.previous.addEventListener('click', function() {
-            this.activeSlideIndex = this.activeSlideIndex === 0 ? this.slidesLength - 1 : this.activeSlideIndex - 1;
-            this.showSlide();
-        }.bind(this));
+        this.toggler.addEventListener('click', this.handleClickOnToggler.bind(this));
     },
 
-    showSlide: function() {
-        this.slides.forEach(function(e) {
-            e.classList.remove('active');
-        });
-        this.slides[this.activeSlideIndex].classList.add('active');
+    render: function() {
+        // внесет изменения в DOM
+        // 1. Показать нужный слайд
+        // 2. Подсветить нужный pagination button
+    },
+
+    handleClickOnNext: function(e) {
+        // write your code here
+    },
+
+    handleClickOnPrevious: function(e) {
+        // write your code here
+    },
+
+    handleClickOnToggler: function(e) {
+        // write your code here
+    },
+
+    handleClickOnPaginationButton: function(e) {
+        // write your code here
     }
 };
-banner.init();
+banner2.subscribe();
+
+// var banner = {
+//     slides: document.querySelectorAll('.slide'),
+//     slidesLength: document.querySelectorAll('.slide').length,
+//     activeSlideIndex: 0,
+//     next: document.querySelector('.next'),
+//     previous: document.querySelector('.previous'),
+//
+//     init: function() {
+//         this.next.addEventListener('click', function() {
+//             console.log(banner.activeSlideIndex)
+//             this.activeSlideIndex = this.activeSlideIndex === this.slidesLength - 1 ? 0 : this.activeSlideIndex + 1;
+//             this.showSlide();
+//         }.bind(this));
+//
+//         this.previous.addEventListener('click', function() {
+//             this.activeSlideIndex = this.activeSlideIndex === 0 ? this.slidesLength - 1 : this.activeSlideIndex - 1;
+//             this.showSlide();
+//         }.bind(this));
+//     },
+//
+//     showSlide: function() {
+//         this.slides.forEach(function(e) {
+//             e.classList.remove('active');
+//         });
+//         this.slides[this.activeSlideIndex].classList.add('active');
+//     }
+// };
+// banner.init();
 
 
 
-var pagination = {
-    buttons: document.querySelectorAll('.button-pagination'),
-    slides: document.querySelectorAll('.banner-text'),
-
-    init: function () {
-        this.buttons.forEach(function(el) {
-            el.addEventListener('click', function(e) {
-                this.buttonsDeleteClass();
-                this.buttonAddActive(e);
-                this.findIndex();
-                this.showSlide();
-            }.bind(this))
-        });
-    },
-
-    buttonsDeleteClass: function() {
-        this.buttons.forEach(function(e) {
-            e.classList.remove('active');
-        });
-    },
-
-    buttonAddActive: function(e) {
-        e.target.classList.add('active');
-    },
-
-    findIndex: function() {
-        banner.activeSlideIndex = Number(document.querySelector('.button-pagination.active').dataset.number);
-    },
-
-    showSlide: function() {
-        this.slides.forEach(function(e) {
-            e.classList.remove('active');
-        });
-        this.slides[banner.activeSlideIndex].classList.add('active');
-    }
-};
-pagination.init();
-
-
-
-var switchButton = {
-    buttonTurn: document.querySelector('.button-turn'),
-    buttonExpand: document.querySelector('.button-expand'),
-    moduleBanner: document.querySelector('.module-banner'),
-
-    init: function() {
-        this.buttonExpand.addEventListener('click', function() {
-            this.showBanner();
-        }.bind(this));
-
-        this.buttonTurn.addEventListener('click', function() {
-            this.hideBanner();
-        }.bind(this));
-    },
-
-    showBanner: function() {
-        this.moduleBanner.style.display = 'block';
-        this.buttonExpand.style.display = 'none';
-        this.buttonTurn.style.display = 'block';
-    },
-
-    hideBanner: function() {
-        this.moduleBanner.style.display = 'none';
-        this.buttonTurn.style.display = 'none';
-        this.buttonExpand.style.display = 'block';
-    }
-};
-switchButton.init()
+// var pagination = {
+//     buttons: document.querySelectorAll('.pagination button'),
+//     slides: document.querySelectorAll('.slide'),
+//
+//     init: function () {
+//         this.buttons.forEach(function(el) {
+//             el.addEventListener('click', function(e) {
+//                 this.buttonsDeleteClass();
+//                 this.buttonAddActive(e);
+//                 this.findIndex();
+//                 this.showSlide();
+//             }.bind(this))
+//         }.bind(this));
+//     },
+//
+//     buttonsDeleteClass: function() {
+//         this.buttons.forEach(function(e) {
+//             e.classList.remove('active');
+//         });
+//     },
+//
+//     buttonAddActive: function(e) {
+//         e.target.classList.add('active');
+//     },
+//
+//     findIndex: function() {
+//         banner.activeSlideIndex = Number(document.querySelector('.button-pagination.active').dataset.number);
+//     },
+//
+//     showSlide: function() {
+//         this.slides.forEach(function(e) {
+//             e.classList.remove('active');
+//         });
+//         this.slides[banner.activeSlideIndex].classList.add('active');
+//     }
+// };
+// pagination.init();
 
 
+
+// var switchButton = {
+//     buttonTurn: document.querySelector('.button-turn'),
+//     buttonExpand: document.querySelector('.button-expand'),
+//     moduleBanner: document.querySelector('.module-banner'),
+//
+//     init: function() {
+//         this.buttonExpand.addEventListener('click', function() {
+//             this.showBanner();
+//         }.bind(this));
+//
+//         this.buttonTurn.addEventListener('click', function() {
+//             this.hideBanner();
+//         }.bind(this));
+//     },
+//
+//     showBanner: function() {
+//         this.moduleBanner.style.display = 'block';
+//         this.buttonExpand.style.display = 'none';
+//         this.buttonTurn.style.display = 'block';
+//     },
+//
+//     hideBanner: function() {
+//         this.moduleBanner.style.display = 'none';
+//         this.buttonTurn.style.display = 'none';
+//         this.buttonExpand.style.display = 'block';
+//     }
+// };
+// switchButton.init();
