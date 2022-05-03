3d52ff84216669574d0bfc6586d8be8d4b0674a0
igorbr <igor.bronovytskyi@playtech.com>
2022-05-03 17:37:33 +0300

    backbone_view

--- backbone_view/js/index.js
+++ backbone_view/js/index.js
@@ -1,70 +1,59 @@
-//----------------------------------------------------AppView
 var AppView = Backbone.View.extend({
     initialize() {
-        this.$el.addClass('app');
-        this.$el.appendTo('body');
-        this.render();
+        $('body').append(this.render().el);
     },
 
-    tmplFnApp: $('#app-template').html(),
+    className: 'app',
 
     render() {
-        this.$el.html(doT.template(this.tmplFnApp));
+        this.$el.html(doT.template($('#app-template').html()));
+
+        this.$('.main').append(new Module1().render().el);
+        this.$('.main').append(new Module1().render().el);
+        this.$('.main').append(new Module1().render().el);
+        this.$('.aside').append(new Module1().render().el);
+        this.$('.aside').append(new Module2().render().el);
+        this.$('.aside').append(new Module2().render().el);
+        this.$('.aside').append(new Module2().render().el);
+
+        return this;
+    }
+});
+
+var Module1 = Backbone.View.extend({
+    events: {
+        'click button': 'onClick'
+    },
+
+    className: 'module1',
+
+    onClick() {
+        this.$el.toggleClass('outline');
+    },
+
+    render() {
+        this.$el.html(doT.template($('#module1-template').html()));
+
+        return this;
+    }
+});
+
+var Module2 = Backbone.View.extend({
+    events: {
+        'click button': 'onClick'
+    },
+
+    className: 'module2',
+
+    onClick() {
+        this.$el.toggleClass('bg');
+    },
+
+    render() {
+        this.$el.html(doT.template($('#module2-template').html()));
+
         return this;
     }
 });
 
 var appView = new AppView;
-
-//----------------------------------------------------MainView
-var MainView = Backbone.View.extend({
-    initialize() {
-        this.$el.addClass('main');
-        $('header').after(this.el);
-        this.render();
-    },
-
-    tmplFnMain: $('#module1-template').html(),
-
-    events: {
-        'click .module1': 'toggleBorder'
-    },
-
-    toggleBorder(e) {
-        $(e.currentTarget).toggleClass('outline');
-    },
-
-    render() {
-        this.$el.html(doT.template(this.tmplFnMain)([0, 1, 2, 3]));
-        return this;
-    }
-})
-
-var mainView = new MainView;
-
-//----------------------------------------------------AsideView
-var AsideView = Backbone.View.extend({
-    initialize() {
-        this.$el.addClass('aside');
-        $('footer').before(this.el);
-        this.render();
-    },
-
-    tmplFnAside: $('#module2-template').html(),
-
-    events: {
-        'click .module2': 'toggleBackground'
-    },
-
-    toggleBackground(e) {
-        $(e.currentTarget).toggleClass('bg');
-    },
-
-    render() {
-        this.$el.html(doT.template(this.tmplFnAside)([0, 1, 2]));
-        return this;
-    }
-})
-
-var asideView = new AsideView;
-
