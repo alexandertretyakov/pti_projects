723fee333e371e441f00f5083f0d54d4e27cb6c7
igorbr <igor.bronovytskyi@playtech.com>
2022-09-06 10:51:38 +0300

    js add

---
+++ js4/functions_part_5.js
@@ -0,0 +1,28 @@
+// Создать функции lastIndexOf, shift, getMatrixSum из файла js/functions_part_1 используя цикл for
+
+
+
+
+// Перепиши вызовы функций ниже используя call или apply:
+// [1,3,5,7].includes(3);
+// [1,3,5,7].indexOf(5);
+// [1,3,5,7].join('/');
+
+
+
+
+// Создать функцию sumOfAllArguments которая принимает произвольное количество чисел и возвращает их сумму.
+// Пример работы:
+// sumOfAllArguments(2, 2, 3);
+// => 7
+// sumOfAllArguments(2, 2, 3, 3, 10);
+// => 20
+
+
+
+
+// Познакомиться с работой следующих встроенных свойств, функций и методов
+// Math: abs, ceil, floor, max, min, pow, random, round, sqrt, trunc
+// JSON: stringify, parse
+
+// Привести примеры использования ниже
