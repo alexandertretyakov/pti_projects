f963ff2bca78bc99e50fbd27ffe7c0e5b17ad554
oleksandrtr <tas19tas@gmail.com>
2022-09-08 19:33:16 +0300

    doT api

--- dot_api/js/task_dot_api.js
+++ dot_api/js/task_dot_api.js
@@ -1,5 +1,5 @@
 //Games
-$('.add-games').on('click', function()  {
+$('.add-games').on('click', function() {
     var gameTemplate = $('#game-template').html();
     $('#games').html(doT.template(gameTemplate)(games));
 });
@@ -7,17 +7,17 @@ $('.add-games').on('click', function()  {
 
 //Foods
 var navTmplFn = doT.template($('#nav-template').html());
-$('.btn-nav-ru').on('click', function()  {
+$('.btn-nav-ru').on('click', function() {
     $('#nav').html(navTmplFn(navRu));
 });
 
-$('.btn-nav-en').on('click', function()  {
+$('.btn-nav-en').on('click', function() {
     $('#nav').html(navTmplFn(navEn));
 });
 
 
 //Whores
-$('.add-whores').on('click', function()  {
+$('.add-whores').on('click', function() {
     var whoreTemplate = $('#whore-template').html();
     $('#whores').html(doT.template(whoreTemplate)(whores));
 });
